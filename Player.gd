d747970d0ac71e24f5d5d34378b0dcfa49454739
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-26 16:33:04 +1030

    e

--- Player.gd
+++ Player.gd
@@ -7,14 +7,34 @@ var lastDir = "+x"
 func _process(delta):
 	if Input.is_action_pressed("left"):
 		vel.x -= speed
+		lastDir = "-x"
+		$Player.scale.x = -1
 	if Input.is_action_pressed("right"):
 		vel.x += speed
-	vel.x *= 0.5/(delta+1)
+		lastDir = "+x"
+		$Player.scale.x = 1
+	if Input.is_action_pressed("up"):
+		vel.y -= speed
+		lastDir = "+y"
+		$Player.scale.x = 1
+	if Input.is_action_pressed("down"):
+		vel.y += speed
+		lastDir = "-y"
+		$Player.scale.x = 1
+	vel *= 0.5/(delta+1)
 	
 	var anim = "Idle"
 	if lastDir == "+y":
-		anim = "IdleFront"
-	elif lastDir == "-y":
 		anim = "IdleBack"
+	elif lastDir == "-y":
+		anim = "IdleFront"
 	
+	if abs(vel.x) > speed/5:
+		anim = "Run"
+	if vel.y > speed/5:
+		anim = "RunFront"
+	if vel.y < -speed/5:
+		anim = "RunBack"
+	
+	$AnimationPlayer.play(anim)
 	move_and_slide(vel)
