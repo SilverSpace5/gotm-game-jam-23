6f818b7b7205174419708dc58ec460f75bbcf3a1
akaade <117557148+akaade@users.noreply.github.com>
2022-12-04 10:28:52 +1030

    hnv

---
+++ gangster.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+
+
+func _ready():
+	$AnimationPlayer.play("idle");
+	yield(get_tree().create_timer(5),"timeout");
+	$AnimationPlayer.play("finger");
+	$AnimationPlayer.play("hehe");
+	yield(get_tree().create_timer(10),"timeout");
+	$AnimationPlayer.play("idle");
